0fa30fdbb9fc0efde972adf13b8fcc545260969f
vtu26660-cell <vtu26660@veltech.edu.in>
2026-04-01 10:55:59 +0530

    Time: 61 ms (68.25%), Space: 107.1 MB (55.51%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        
+        Stack<Integer> stack = new Stack<>(); // stores indices
+        
+        for (int i = 0; i < n; i++) {
+            
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            
+            stack.push(i);
+        }
+        
+        return result;
+    }
+}
